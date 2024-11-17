d1b3c9ac9cc91c9fd320acad258bb86cbb00d02b
alyxx3ds <alessandrorick690@gmail.com>
2024-11-17 01:47:22 +0100

    develop02

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Entry 
+{
+    public string _date;
+    public string _prompt;
+    public string _response;
+
+    public void GenerateDate()
+    {
+        DateTime theCurrentTime = DateTime.Now;
+        _date = theCurrentTime.ToShortDateString();
+    }
+
+    public void GeneratePrompt()
+    {
+        List<string> prompts = new List<string>()
+        {
+            "how did you feel emotionally today?",
+            "What are you looking forward to?",
+            "What are you grateful for today?",
+            "What did you learn today?",
+        };
+
+        Random rand = new Random();
+        int randomIndex = rand.Next(prompts.Count);
+        _prompt = prompts[randomIndex];
+        Console.WriteLine(_prompt);
+    }
+
+    public void GetResponse()
+    {
+        Console.Write("> ");
+        _response = Console.ReadLine();
+    }
+}
